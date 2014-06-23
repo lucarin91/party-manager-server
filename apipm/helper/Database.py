4ee42c60c7c223f80270359564ac3a995c7cac70
lucarin91 <lucarin91@gmail.com>
2014-06-23 18:45:55 +0200

    sistemare errore method not allowed

--- apipm/helper/Database.py
+++ apipm/helper/Database.py
@@ -1,16 +1,14 @@
-import psycopg2, psycopg2.extras, collections
+import psycopg2, psycopg2.extras, collections, os
 
 sql = psycopg2.connect(
     database='pm_database',
     user='apipm',
     password='geronsi',
     host='localhost',
-)
+) if os.environ['WSGI_ENV']=='deploy.py' else None
 
 def delUtenteFromEvent(idEvento,idFacebook):
-	cur = sql.cursor()
-	cur.execute("DELETE FROM evento WHERE id_evento=%s and id_user=%s", (idEvento,idFacebook))
-    cur.execute("""DELETE FROM rispose 
-                    WHERE id_attributo 
-                    IN (select id_attributo from attributi where id_evento=%s) and id_user=%s""", (idEvento,idFacebook))     
+    cur = sql.cursor()
+    cur.execute("DELETE FROM evento WHERE id_evento=%s and id_user=%s", (idEvento,idFacebook))
+    cur.execute("DELETE FROM rispose WHERE id_attributo IN (select id_attributo from attributi where id_evento=%s) and id_user=%s", (idEvento,idFacebook))     
     sql.commit()
