2ceebbfd8d6dead2ba7034f255648e4e44467b87
lucarin91 <lucarin91@gmail.com>
2014-06-23 17:48:31 +0200

    prove

--- apipm/deploy.py
+++ apipm/deploy.py
@@ -1,9 +1,2 @@
-import psycopg2
-DEBUG=False
-SQL = psycopg2.connect(
-    database='pm_database',
-    user='apipm',
-    password='geronsi',
-    host='localhost',
-)
+DEBUG=True
 WHERE='deployment'
