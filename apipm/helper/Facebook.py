4b9b857da42b66ab13f7848aca1e5fe984a70154
lucarin91 <lucarin91@gmail.com>
2014-06-24 13:22:02 +0200

    sistemo gli errori sugli elimina

--- apipm/helper/Facebook.py
+++ apipm/helper/Facebook.py
@@ -1,16 +1,26 @@
 import json
 import urllib2
+from urllib2 import HTTPError
 
 
 def getFacebookName(idFacebook):
-    if isinstance(idFacebook, str):
-        print 'getFacebookName: is string ' + idFacebook
-        ris = json.load(urllib2.urlopen('http://graph.facebook.com/' + idFacebook))
-        return ris['name']
-    else:
-        print 'getFacebookName: is list'
-        ris = []
-        for u in idFacebook:
-            ris.append({'id_user': u,
-                        'name': json.load(urllib2.urlopen('http://graph.facebook.com/' + u))['name']})
-        return str(ris)
+    try:
+        if isinstance(idFacebook, str):
+            print 'getFacebookName: is string ' + idFacebook
+            ris = json.load(urllib2.urlopen('http://graph.facebook.com/' + idFacebook))
+            return ris['name']
+
+        elif isinstance(idFacebook, list):
+            print 'getFacebookName: is list'
+            ris = []
+            for u in idFacebook:
+                ris.append({'id_user': u,
+                            'name': json.load(urllib2.urlopen('http://graph.facebook.com/' + u))['name']})
+            return str(ris)
+
+        else:
+            print 'parametro errato'
+            return None
+            
+    except HTTPError:
+        print 'Could not download page'
