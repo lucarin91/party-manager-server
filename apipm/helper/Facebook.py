ea0199b0b71c12915813f22ce8289a1cf7f94859
lucarin91 <lucarin91@gmail.com>
2014-06-24 13:11:43 +0200

    sistemo gli errori sugli elimina

--- apipm/helper/Facebook.py
+++ apipm/helper/Facebook.py
@@ -4,9 +4,11 @@ import urllib2
 
 def getFacebookName(idFacebook):
     if isinstance(idFacebook, str):
+        print 'getFacebookName: is string ' + idFacebook
         ris = json.load(urllib2.urlopen('http://graph.facebook.com/' + idFacebook))
         return ris['name']
     else:
+        print 'getFacebookName: is list'
         ris = []
         for u in idFacebook:
             ris.append({'id_user': u,
